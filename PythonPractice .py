9b3347633409336a52194a7ad6b4425e8550c63a
Akryaz <akryazbim@gmail.com>
2026-03-25 17:09:15 +0530

    Add practice file

---
+++ PythonPractice .py
@@ -0,0 +1,468 @@
+
+
+#'He\'s the "King"' 
+
+#ctrl + /
+
+# a = 10
+# b = r"E:\Python Revit API Training\Working Files\nRevitApiTraining-March-2026"
+# xyz = 500
+
+
+# a = "-123"
+
+# x = 123
+# y = 456.9999999999
+
+# b = float(x)  
+# c = int(y)
+
+
+
+
+
+
+# a = "Hi"
+
+# #2*3 = 2+2+2
+
+# print(a*0.5)  # a*1/2 => a/2
+
+#**
+#//
+#%
+
+
+#AND -> both conditions must be true
+#OR -> at least one condition must be true
+
+
+
+# a = 10
+
+# if a >= 5:
+#     print("Yes")
+#     print("IN in if")
+# else:
+#     print("No")
+#     print("IN in else")
+#     print("IN in else")
+
+
+
+
+# abc = 9
+
+# if abc%2 == 0:
+#     print ("Even")
+# else:
+#     print ("Odd")
+
+
+# if abc%2 != 0:
+#     print("Odd")
+# else:
+#     print("Even")
+
+
+# if abc%2 == 0:
+#     print("Even")
+# if abc%2 == 1:
+#     print("odd")
+
+
+# x = 10
+# if x & 2:
+#     print("even")
+# else:
+#     print("odd")
+
+
+
+
+# a = 505
+# b = 501
+
+# if a>b:
+#     print("A is greatest")
+
+# print("   ")
+
+
+# if a>b:
+#     print("A is greatest")
+# elif b>a: 
+#     print("B is greatest")
+# elif a == b:
+#     print("A and B are equal")
+
+    
+# print("This is outside of if-else block")
+
+
+
+
+
+# x = 12
+# y = 53
+
+# if x>y:
+#     print("X is greater than Y")
+#     if x%2 == 0:
+#         print("X is even")
+#         if x%3 == 0:
+#             print("X is divisible by 3")
+#         else:
+#             print("X is not divisible by 3")
+#     else:
+#         print("X is odd")
+# elif y>x:
+#     print("Y is greater than X")    
+#     if y%2 == 0:
+#         print("Y is even")
+#     else:
+#         print("Y is odd")
+# else:
+#     print("X and Y are equal")
+#     if x%2 == 0:
+#         print("X and Y are even")
+#     else:
+#         print("X and Y are odd")
+
+
+# x = 10
+# y = 5
+
+# z = 0
+
+# if x>y:
+#     print("X is greater than Y")
+#     z = x
+# elif y>x:
+#     print("Y is greater than X")
+#     z = y
+# else:
+#     print("X and Y are equal")
+#     z = x
+
+# if z%2 == 0:
+#     print("Greatest number is even")
+# else:
+#     print("Greatest number is odd")
+
+
+
+
+
+
+
+# name = "Pubesh"      
+
+# if "dsgsdg" not in name:
+
+#     print("Yes")
+# else:    
+#     print("No")
+
+
+# if (!name.Conatins("dsgsdg"))
+# {
+#     print("Yes");
+# }
+# else
+# {
+#     print("No");
+# }
+
+
+# a = 93
+# b = 91
+# c = 10
+# z = 0
+# # Finding which is the greatest number
+# if a>b and a>c:
+#     print(a, "is the greatest number")
+#     z=a
+# elif b>a and b>c:
+#     print(b, "is the greatest number")
+#     z=b
+# else:
+#     print(c, "is the greatest number")
+#     z=c
+# # Finding if the number is odd or even    
+# if z%2 ==0:
+#     print("This is a even number")
+# else:
+#     print("This is a odd number")
+    
+# # Finding if the number is greater than,eaual to or less than 100
+# if z>100:
+#     print("The number is greater than 100")
+# elif z==100:
+#     print("The number is equal to 100")
+# else:
+#     print("The number is less than 100")
+
+
+# a=93
+# b=910
+# c=10
+
+# x = max(a,b,c)
+
+# print(max(a,b,c))
+
+
+# a = 10
+# b = 20
+# c = 30
+
+
+# print(max(a, b, c))
+# print(a == b == c)
+# for name, val in [("a", a), ("b", b), ("c", c)]:
+#     print(f"{name} is {'even' if val % 2 == 0 else 'odd'}")
+
+
+# for name, val in [("a", a), ("b", b), ("c", c)]:
+#     if val > 100:
+#         print(f"{name} is greater than 100")
+#     elif val < 100:
+#         print(f"{name} is less than 100")
+#     else:
+#         print(f"{name} equals 100")
+
+
+# a=100
+# b=100
+# c=300
+
+# if a>b and a>c:
+#     print ("a is greatest")
+# elif b>a and b>c:
+#     print ("b is greatest")
+# else:
+#     print ("c is greatest")
+# else:
+#     print ("a and b and c are equal")
+
+
+
+
+# if a==b or b==c or c==a:
+#     print ("a and b and c are equal")
+# else:
+#     print ("a and b and c are not equal")
+# if a%2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+# if b%2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+# if c%2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+
+
+# a = 85   # Score for Student A
+# b = 55   # Score for Student B
+# c = 40   # Score for Student C
+# for name, score in [("A", a), ("B", b), ("C", c)]:
+#     print(f"Student {name} (Score: {score})", end=" → ")
+#     if score >= 80:
+#         print("Excellent! Grade: A")
+#     elif score >= 60:
+#         print("Good. Grade: B")
+#     elif score >= 50:
+#         print("Average. Grade: C")
+#     else:
+#         print("Failed. Grade: F")
+
+
+# a=10
+# b=20
+# c=30
+# if (a>b and b>c):
+#     print(a)
+# elif(b>c,b>a):
+#     print(b)
+# else:
+#     print(c)
+
+
+# a = 10
+# b = 50
+
+# if a>b: 
+#     print("A is greater than B")    
+# else:
+#     print("B is greater than A")
+
+
+# a = 30
+# if a>0 and a<=2:
+#     print (25)
+# elif a>2 and a<=3:
+#     print (32)
+# elif a>3 and a<=5:
+#     print (40)
+# elif a>5 and a<=10:
+#     print (50)
+# elif a>10 and a<=30:
+#     print (65)
+# elif a>30 and a<=60:
+#     print (80)
+# elif a>60 and a<=100:
+#     print (90)
+# elif a>100:
+#     print (100)
+# else:
+#     print ("None")
+
+
+# a = 50
+
+# if a%2==0:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+
+# a=10
+# b=a/2
+# if (b==int):
+#     print("even")
+# else:
+#     print ("odd")
+
+# print(type(2)==int)
+
+# y = [10,20]
+# x = [10,20,y]
+# x[-1]
+# testList = [100, 4.9, "abc", True, 10+15, "xyz", x]
+
+
+# lastItem = testList[-1]
+
+# a = testList[6]
+
+# print(a[-1][0])
+
+
+# x = 50
+# y = 50
+
+# a = [10,x,y,50,50] #list
+
+# b = (10,50,50,50,50,60,75,100) #tuple
+
+
+# c = {10,10,25,50,50,50,50,10,10, "abc"} #set
+
+
+# d = set(a)
+
+
+# name = "Prasanta"
+
+
+# names = ["Prasanta", "Pubesh", "Gaman", "Harsh", "Sandip", "Venkat", "Dnyan", "Saravana"]
+
+
+
+
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+# print(names[4])
+
+
+
+
+# i = 1
+# while i <= 10:
+#     if i%2 == 0:        
+#         print(i, "is even")
+#     else:
+#         print(i, "is odd")
+#     i += 1
+
+
+# sum = 0
+# i = 1
+# while i <= 1000:
+#     sum = sum+i
+#     i += 1
+
+# print(sum)
+
+
+
+names = ["Prasanta", "Pubesh", "Gaman", "Harsh", "Sandip", "Venkat", "Dnyan", "Saravana"]
+
+for name in names:
+    #if "s" in name or "S" in name:
+    nameInLowerCase = name.upper()
+    if "S" in nameInLowerCase:
+        print(name)
+
+names.append("nfdlanh")
+
+#continue
+#break
+
+# i = 0
+# while i <= len(names)-1:
+#     print(names[i])
+#     i += 1
+
+# i = 0
+# while i in range(len(names)):
+#     print(names[i])
+#     i += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
